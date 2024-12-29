32c2527593a2b7ed00635855c14501f2332265b2
Kazzzemir <s1ckwell@mail.ru>
2024-12-29 17:02:53 +0300

    sockets fix 1

--- src/routes/loaders.ts
+++ src/routes/loaders.ts
@@ -25,6 +25,10 @@ export const rootPageLoader = async () => {
     setStatusSocket(statusSocket);
     setUsersSocket(usersSocket);
 
+    socket.on('connnection', () => {
+      console.log('connected');
+    });
+
     return null;
   } catch (error) {
     if (axios.isAxiosError(error)) {
